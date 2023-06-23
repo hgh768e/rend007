4b41ebe7bac65ed8d966e624013c986a4e66bfa6
hgh768e <103629748+hgh768e@users.noreply.github.com>
2023-06-23 08:48:28 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 export UUID=22da7537-3ffb-4ff0-99eb-cf640310b65b
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.bing.com
+export ProxySite=www.github.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
